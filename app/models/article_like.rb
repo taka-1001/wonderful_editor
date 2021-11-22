7639d09ef6f0091df762d5db61247960aa01584d
d-takahashi <tkjundai@gmail.com>
2021-11-22 16:32:32 +0900

    userlike model の名前の変更

--- app/models/article_like.rb
+++ app/models/article_like.rb
@@ -1,4 +1,4 @@
-class UserLike < ApplicationRecord
+class ArticleLike < ApplicationRecord
   belongs_to :user
   belongs_to :article
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_041549) do
+ActiveRecord::Schema.define(version: 2021_11_22_072622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_article_likes_on_article_id"
+    t.index ["user_id"], name: "index_article_likes_on_user_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,15 +41,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_041549) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "user_likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "article_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["article_id"], name: "index_user_likes_on_article_id"
-    t.index ["user_id"], name: "index_user_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,9 +68,9 @@ ActiveRecord::Schema.define(version: 2021_11_22_041549) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "article_likes", "articles"
+  add_foreign_key "article_likes", "users"
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
-  add_foreign_key "user_likes", "articles"
-  add_foreign_key "user_likes", "users"
 end
