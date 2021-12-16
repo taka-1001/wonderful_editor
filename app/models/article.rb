f392ae43d00a4f76dd3c353012e8ef9e2cf2d6d9
d-takahashi <tkjundai@gmail.com>
2021-12-16 16:17:23 +0900

    enum を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   validates :title, presence: true, length: { maximum: 15 }
+  enum status: { draft: "draft", published: "published" }
 end
