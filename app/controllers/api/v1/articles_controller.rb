e371a32bd4feb0f9f6b35d4ebaaaabbe2db908ef
d-takahashi <tkjundai@gmail.com>
2021-12-20 14:43:34 +0900

    公開記事を取得できるように変更

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,24 +2,24 @@ class Api::V1::ArticlesController < Api::V1::BaseApiController
   before_action :authenticate_user!, only: [:create, :update, :destroy]
 
   def index
-    articles = Article.order(updated_at: :desc)
+    articles = Article.published.order(updated_at: :desc)
     render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article, serializer: Api::V1::ArticleSerializer
+    article = Article.published.find(params[:id])
+    render json: article
   end
 
   def create
     article = current_user.articles.create!(article_params)
-    render json: article, serializer: Api::V1::ArticleSerializer
+    render json: article
   end
 
   def update
     article = current_user.articles.find(params[:id])
     article.update!(article_params)
-    render json: article, serializer: Api::V1::ArticleSerializer
+    render json: article
   end
 
   def destroy
@@ -30,6 +30,6 @@ class Api::V1::ArticlesController < Api::V1::BaseApiController
   private
 
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
